6b93d8afa4d94bc8c76e3ca38a6798349c0edd93
Iaroslav Uliantcev <doudou239@gmail.com>
2025-05-13 23:31:20 +0200

    Add save of the cropped image. Change representation of the blocks. Specify types.

--- src/components/CropperBlock.tsx
+++ src/components/CropperBlock.tsx
@@ -1,44 +1,68 @@
-import React, {useState} from "react";
-import Cropper from "react-cropper";
+import React, {ChangeEvent, createRef, DragEvent, useState} from "react";
+import Cropper, {ReactCropperElement} from "react-cropper";
 import "cropperjs/dist/cropper.css";
 import "./CropperBlock.css";
 
-const defaultSrc =
-    "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
-
 export const CropperBlock: React.FC = () => {
-    const [image, setImage] = useState(defaultSrc);
-    const [cropData, setCropData] = useState("#");
-    const [cropper, setCropper] = useState<any>();
-    const onChange = (e: any) => {
+    const [image, setImage] = useState<string | undefined>(undefined);
+    const cropperRef = createRef<ReactCropperElement>();
+
+    const onChange = (e: ChangeEvent<HTMLInputElement> | DragEvent<HTMLDivElement>) => {
         e.preventDefault();
-        let files;
-        if (e.dataTransfer) {
-            files = e.dataTransfer.files;
-        } else if (e.target) {
-            files = e.target.files;
+        let files: FileList | null = null;
+        if (e.type === "change") {
+            const target = e.target as HTMLInputElement;
+            files = target.files;
+        } else if (e.type === "drop") {
+            const target = e as DragEvent<HTMLDivElement>;
+            files = target.dataTransfer?.files || null;
         }
-        const reader = new FileReader();
-        reader.onload = () => {
-            setImage(reader.result as any);
-        };
-        reader.readAsDataURL(files[0]);
+
+        if (files && files[0]) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                if (typeof reader.result === "string") {
+                    setImage(reader.result);
+                }
+            };
+            reader.readAsDataURL(files[0]);
+        }
     };
 
     const getCropData = () => {
-        if (typeof cropper !== "undefined") {
-            setCropData(cropper.getCroppedCanvas().toDataURL());
+        try {
+            const cropper = cropperRef.current?.cropper;
+            if (cropper) {
+                cropper.getCroppedCanvas({
+                    width: 200,
+                    height: 200,
+                }).toBlob((blob) => {
+                    if (!blob) return;
+                    const url = URL.createObjectURL(blob); // create the URL on Blob
+                    const a = document.createElement("a");
+                    a.href = url;
+                    a.download = "cropped.jpg"; // file's saved name
+                    document.body.appendChild(a);
+                    a.click();
+                    document.body.removeChild(a);
+                    URL.revokeObjectURL(url); // clear memory cache
+                }, "image/jpeg", 0.9);
+            }
+        } catch (e) {
+            // TODO add an error action
+            console.error("crop error")
         }
-    };
+    }
 
     return (
         <div>
             <div style={{width: "100%"}}>
                 <input type="file" onChange={onChange}/>
-                <button>Use default img</button>
-                <br/>
-                <br/>
-                <Cropper
+                {image && <>
+                  <br/>
+                  <br/>
+                  <Cropper
+                    ref={cropperRef}
                     style={{height: 400, width: "100%"}}
                     zoomTo={0.5}
                     initialAspectRatio={1}
@@ -51,33 +75,30 @@ export const CropperBlock: React.FC = () => {
                     responsive={true}
                     autoCropArea={1}
                     checkOrientation={false}
-                    onInitialized={(instance) => {
-                        setCropper(instance);
-                    }}
                     guides={true}
-                />
+                  />
+                </>}
             </div>
-            <div>
-                <div className="box" style={{width: "50%", float: "right"}}>
-                    <h1>Preview</h1>
-                    <div
-                        className="img-preview"
-                        style={{width: "100%", float: "left", height: "300px"}}
-                    />
-                </div>
+            {image && <div>
+              <div className="box" style={{width: "50%", float: "right"}}>
+                <h1>Preview</h1>
                 <div
-                    className="box"
-                    style={{width: "50%", float: "right", height: "300px"}}
-                >
-                    <h1>
-                        <span>Crop</span>
-                        <button style={{float: "right"}} onClick={getCropData}>
-                            Crop Image
-                        </button>
-                    </h1>
-                    <img style={{width: "100%"}} src={cropData} alt="cropped"/>
-                </div>
-            </div>
+                  className="img-preview"
+                  style={{width: "100%", float: "left", height: "300px"}}
+                />
+              </div>
+              <div
+                className="box"
+                style={{width: "50%", float: "right", height: "300px"}}
+              >
+                <h1>
+                  <span>Crop</span>
+                  <button style={{float: "right"}} onClick={getCropData}>
+                    Crop Image
+                  </button>
+                </h1>
+              </div>
+            </div>}
             <br style={{clear: "both"}}/>
         </div>
     );
